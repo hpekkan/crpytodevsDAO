4c8c149beff776423448c4928563349a0722caf0
hpekkan <hpekkan@etu.edu.tr>
2022-07-13 21:48:00 +0300

    second

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xF2b581570Eb8bA2afe5c89962a9FbaC00B6864eE";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
